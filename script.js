9427cebd99b401d3f270eb0f1584baaa00222f4c
Abdullah <abdullah056644@hotmail.com>
2022-08-18 16:09:40 +0300

    Add deleteDigit function

--- script.js
+++ script.js
@@ -17,3 +17,16 @@ function insertNumber(numberButton) {
         }
     }
 }
+
+function deleteDigit() {
+
+    const result = document.querySelector('.result');
+    if (result.getAttribute('class').includes('equ')) {
+        const currComputing = document.querySelector('.current-computing');
+        currComputing.textContent = '';
+        result.classList.remove('equ');
+    } else {
+        result.textContent = result.textContent.slice(0, -1);
+        if (result.textContent == '') result.textContent = 0;
+    }
+}
